73dd1de54efe4938ef152fe9fd66ff75493514d6
Carolina Azofeifa <mcazofeifa@gmail.com>
2017-05-14 17:11:10 -0600

    fix menu notebook color js

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,15 @@
 (function(){
 
 
-document.getElementById("buttonNote").addEventListener("click",editNote);
+document.getElementById("buttonNote").addEventListener("click",showNotesEditor);
 document.getElementById("buttonNotebook").addEventListener("click",showMenuNotebooks);
+document.getElementById("addNoteBook").addEventListener("click",addNoteBook);
 
 notesModal = document.getElementById("notesModal");
 notebooks = document.getElementById("sectionFile")
+
+inputNameNoteBook= document.getElementById("inputNoteBook");
+
 /*document.getElementById("pantallaprincipal").addEventListener("click",clickonscreen);
 document.getElementById("buttonLib").addEventListener("click",lib);
 document.getElementById("buttonTag").addEventListener("click",tag);
@@ -18,7 +22,6 @@ buttonFile = document.getElementById("buttonLib");
 menuLateralLib = document.getElementById("menuLateralLib");
 menuLateralTag = document.getElementById("menuLateralTag");
 pantallaprincipal = document.getElementById("pantallaprincipal");
-inputBookname= document.getElementById("inputBook");
 
 imgEditar = document.getElementById("imgEditar");
 
@@ -26,7 +29,7 @@ imgEditar = document.getElementById("imgEditar");
 var botonLibSeleccionado=false;
 var botonTagSeleccionado=false;
 */
-function editNote() {
+function showNotesEditor() {
     notesModal.classList.add("notesModal--show");
     document.getElementById("nameNote").focus();
     document.getElementById("mainView").classList.add("mainViewOpaque");
@@ -38,14 +41,43 @@ document.onkeydown = function myFunction(event) {
         //ocultarMenuTag();
         notesModal.classList.remove("notesModal--show");
         document.getElementById("mainView").classList.remove("mainViewOpaque");
-        notebooks.classList.remove("sectionFile--show")
-        
+        notebooks.classList.remove("sectionFile--show")   
     }
 }
 function showMenuNotebooks(){
     notebooks.classList.add("sectionFile--show");
     notesModal.classList.remove("notesModal--show");
-    document.getElementById("mainView").classList.remove("mainViewOpaque");
+    document.getElementById("mainView").classList.add("mainViewOpaque");
+}
+function addNoteBook(){
+    console.log("new book");
+
+    //TITULO DEL NOTEBOOK
+    var name =inputNameNoteBook.value;
+    var h6 = document.createElement("h6");
+    h6.setAttribute("contenteditable", "true");
+    
+    var text = document.createTextNode(name);
+    h6.appendChild(text);
+
+    //ROW
+    var div = document.createElement("div");
+    div.setAttribute("class", "row sectionFile__li");
+
+    //COLUMN
+    var divCol = document.createElement("div");
+    divCol.setAttribute("class", "col-md-12");
+
+    div.appendChild(divCol);
+    divCol.appendChild(h6);
+
+    //LISTA
+    var node = document.createElement("li");
+    node.setAttribute("class", "sectionFile__list");
+
+    node.appendChild(div);
+    document.getElementById("notebooksList").appendChild(node);
+    console.log("agregado");
 }
 
 /*
